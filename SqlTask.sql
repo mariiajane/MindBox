17244929e651aff1457f9d26a3e7ec636476616f
Mariia Pushkareva <m.pushkareva@intetics.com>
2024-01-05 11:52:47 +0100

    script

---
+++ SqlTask.sql
@@ -0,0 +1,11 @@
+﻿
+
+SELECT
+    P.ProductName,
+    ISNULL(C.CategoryName, 'Без категории') AS CategoryName
+FROM
+    Products P
+LEFT JOIN
+    ProductCategory PC ON P.ProductID = PC.ProductID
+LEFT JOIN
+    Categories C ON PC.CategoryID = C.CategoryID;
