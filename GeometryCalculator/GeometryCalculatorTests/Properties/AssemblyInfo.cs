dcfae59cd4fceb58d04f4445dab63ee03e1b7684
Mariia Pushkareva <m.pushkareva@intetics.com>
2024-01-05 11:43:26 +0100

    test adding

---
+++ GeometryCalculator/GeometryCalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GeometryCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeometryCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13bcdd27-1d76-441e-9548-9cff15b89e01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GeometryCalculator/GeometryCalculatorTests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using GeometryLibrary;
+
+namespace GeometryLibrary
+{
+    [TestFixture]
+    public class GeometryCalculatorTests
+    {
+        [Test]
+        public void CalculateCircleArea_ValidRadius_ReturnsCorrectArea()
+        {
+            double radius = 5;
+            double expectedArea = Math.PI * Math.Pow(radius, 2);
+
+            double result = GeometryCalculator.CalculateCircleArea(radius);
+
+            Assert.AreEqual(expectedArea, result, 0.0001);
+
+        }
+
+        [Test]
+        public void CalculateTriangleArea_ValidSides_ReturnsCorrectArea()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+
+            double expectedArea = 6;
+
+            double result = GeometryCalculator.CalculateTriangleArea(sideA, sideB, sideC);
+
+            Assert.AreEqual(expectedArea, result, 0.0001);
+
+
+        }
+
+        [Test]
+        public void IsRightAngledTriangle_RightAngledTriangle_ReturnsTrue()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+
+            bool result = GeometryCalculator.IsRightAngledTriangle(sideA, sideB, sideC);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
